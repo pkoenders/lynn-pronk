6d684de7584f555d6265bfc662ced24d386dd691
Peter Koenders <pkoenders@gmail.com>
2020-04-24 00:28:11 +1200

    forms

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,47 +1,69 @@
-import React from 'react'
-import Layout from '../components/layout'
-import Head from '../components/head'
-
-import contactStyles from './contact.module.scss'
+import React from 'react';
+import ContactBGroundImg from '../components/contact-bground-img'
+import Layout from '../components/layout';
+import Head from '../components/head';
+import contactStyles from './contact.module.scss';
 
 const ContactPage = () => {
   return (
+    <div>
+      <ContactBGroundImg />
+      <Layout>
+        <Head title="Contact" />
+
+        <div className={contactStyles.contactForm}>
+
+        </div>
+
+        <div className={contactStyles.contactFormWrapper}>
 
 
-    <Layout>
+          <h2>Contact Lynn</h2>
+          <p><strong>Thanks for visiting my website!</strong></p>
+          <p>I would love to be contacted to discuss work commissions or just hear your thoughts and ideas.
+I will always reply.</p>
 
+          <div className={contactStyles.contactFormInput}>
+            <form
+              method="post"
+              netlify-honeypot="bot-field"
+              data-netlify="true"
+              name="lynn-pronk-contact"
+            >
+              <input type="hidden" name="bot-field" />
+              <p>
+                <label>
+                  <span>Name (required)</span>
+                  <input type="text" name="name" id="name" />
+                </label>
+              </p>
+              <p>
+                <label>
+                  <span>Email (required)</span>
+                  <input type="email" name="email" id="email" />
+                </label>
+              </p>
+              <p>
+                <label>
+                  <span>Subject</span>
+                  <input type="text" name="subject" id="subject" />
+                </label>
+              </p>
+              <p>
+                <label>
+                  <span>Message</span>
+                  <textarea name="message" id="message" rows="5" />
+                </label>
+              </p>
+              <p>
+                <button type="submit" class="buttonPrimary">Submit</button>
+              </p>
+            </form>
+          </div>
+        </div>
+      </Layout >
+    </div>
+  );
+};
 
-
-
-      <Head title="Contact" />
-      <h2>Contact</h2>
-      <p>The page to contact me.</p>
-
-
-      <form method="post" netlify-honeypot="bot-field" data-netlify="true" name="lynn-pronk-contact">
-        <input type="hidden" name="bot-field" />
-        <label>
-          Name
-    <input type="text" name="name" id="name" />
-        </label>
-        <label>
-          Email
-    <input type="email" name="email" id="email" />
-        </label>
-        <label>
-          Subject
-    <input type="text" name="subject" id="subject" />
-        </label>
-        <label>
-          Message
-    <textarea name="message" id="message" rows="5" />
-        </label>
-        <button type="submit">Send</button>
-        <input type="reset" value="Clear" />
-      </form>
-    </Layout>
-
-  )
-}
-
-export default ContactPage
+export default ContactPage;
